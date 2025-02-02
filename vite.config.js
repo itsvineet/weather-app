55db036e12183e43c86484c025a8df0412c4fd9e
Vineet Khandelwal <vineetkhandelwal29@gmail.com>
2025-02-03 02:06:52 +0530

    debugging

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/weather-app"
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/weather-app"
+// })
+
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    base: "/weather-app",
+    define: {
+      'process.env': env
+    }
+  };
+});
