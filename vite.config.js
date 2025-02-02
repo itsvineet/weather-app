b02cf98995400e71427e8406fc9fdf831d95966b
Vineet Khandelwal <vineetkhandelwal29@gmail.com>
2025-02-03 02:17:54 +0530

    cleaning code

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/weather-app"
-// })
-
-
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
